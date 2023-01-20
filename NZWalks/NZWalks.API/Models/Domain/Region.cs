8c0a2e39a358a3af275bc14ae3032ff524b9c572
seenu7804 <seenu7804@gmail.com>
2023-01-20 08:41:21 +0530

    End of session 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,24 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+        public String Name { get; set; }
+
+        public double Area { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+
+        public long Population { get; set; }
+
+
+        // Navigation property
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
